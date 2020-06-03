Havoc-OS/android_vendor_support

support: Add SystemPropListPreference
@ZeNiXxX: Adapt to AndroidX

Change-Id: If1a478f75bb9c0c0970cc604d4de6bfbc93c26d7

src/com/havoc/support/preferences/ListPreference.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2017-2018 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.support.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class ListPreference extends androidx.preference.ListPreference {
+    private boolean mAutoSummary = false;
+
+    public ListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public ListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(value);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(getEntry(), true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
+    }
+}

src/com/havoc/support/preferences/SystemPropListPreference.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2019 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.support.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class SystemPropListPreference extends ListPreference {
+
+    public SystemPropListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropListPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+        // Instead, we better do
+        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+}

src/com/havoc/support/preferences/SystemPropStore.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright (C) 2019 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.support.preferences;
+
+import android.os.SystemProperties;
+import android.preference.PreferenceDataStore;
+
+public class SystemPropStore extends androidx.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return SystemProperties.getBoolean(key, defValue);
+    }
+
+    public float getFloat(String key, float defValue) {
+        return Float.parseFloat(SystemProperties.get(key, "" + defValue));
+    }
+
+    public int getInt(String key, int defValue) {
+        return SystemProperties.getInt(key, defValue);
+    }
+
+    public long getLong(String key, long defValue) {
+        return SystemProperties.getLong(key, defValue);
+    }
+
+    public String getString(String key, String defValue) {
+        return SystemProperties.get(key, defValue);
+    }
+
+    public void putBoolean(String key, boolean value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putFloat(String key, float value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putInt(String key, int value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putLong(String key, long value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putString(String key, String value) {
+        SystemProperties.set(key, value);
+    }
+
+}

Hyper-Paranoid/android_vendor_pa

support/src/com/hyperparanoid/support/preferences/ListPreference.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2017-2018 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hyperparanoid.support.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class ListPreference extends androidx.preference.ListPreference {
+    private boolean mAutoSummary = false;
+
+    public ListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public ListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void setValue(String value) {
+        super.setValue(value);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(getEntry(), true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
+    }
+}

support/src/com/hyperparanoid/support/preferences/SystemPropListPreference.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2019 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hyperparanoid.support.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class SystemPropListPreference extends ListPreference {
+
+    public SystemPropListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    public SystemPropListPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemPropStore());
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+        // Instead, we better do
+        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+}

support/src/com/hyperparanoid/support/preferences/SystemPropStore.java
@@ -1,0 +1,65 @@
+/*
+ * Copyright (C) 2019 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hyperparanoid.support.preferences;
+
+import android.os.SystemProperties;
+import android.preference.PreferenceDataStore;
+
+public class SystemPropStore extends androidx.preference.PreferenceDataStore
+        implements PreferenceDataStore {
+
+    public boolean getBoolean(String key, boolean defValue) {
+        return SystemProperties.getBoolean(key, defValue);
+    }
+
+    public float getFloat(String key, float defValue) {
+        return Float.parseFloat(SystemProperties.get(key, "" + defValue));
+    }
+
+    public int getInt(String key, int defValue) {
+        return SystemProperties.getInt(key, defValue);
+    }
+
+    public long getLong(String key, long defValue) {
+        return SystemProperties.getLong(key, defValue);
+    }
+
+    public String getString(String key, String defValue) {
+        return SystemProperties.get(key, defValue);
+    }
+
+    public void putBoolean(String key, boolean value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putFloat(String key, float value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putInt(String key, int value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putLong(String key, long value) {
+        SystemProperties.set(key, "" + value);
+    }
+
+    public void putString(String key, String value) {
+        SystemProperties.set(key, value);
+    }
+
+}